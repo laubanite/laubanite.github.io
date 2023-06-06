d681feb505e1519deb2b9b1f58c5e1b8f8b1550c
laubanite <2378840262@qq.com>
2023-06-06 23:11:03 +0800

    Site updated: 2023-06-06 23:11:02

---
+++ js/sharejs.js
@@ -0,0 +1,9 @@
+<div class="social-share"></div>
+
+<!-- <div class="share-component" data-sites="qq,weibo,wechat,douban,twitter,facebook" data-description="Share.js - 一键分享到微博，QQ空间，腾讯微博，人人，豆瓣"></div> -->
+<div class="share-component" data-disabled="google,twitter,facebook" data-description="Share.js - 一键分享到微博，QQ空间，腾讯微博，人人，豆瓣"></div>
+<!--  css & js -->
+<link rel="stylesheet" href="dist/css/share.min.css">
+<script src="dist/js/social-share.min.js"></script>
+
+// 当你使用类名为 `social-share` 时不需要手动初始化
